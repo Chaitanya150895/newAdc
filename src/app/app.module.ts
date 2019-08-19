278c9be3dafdd77171e9134fa4fca734467210a2
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-08-19 18:11:09 +0530

    router-outlet and routing module added || working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewLocationComponent } from './add-location/new-location/new-location.component';
+import { AddLocationComponent } from './add-location/add-location.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewLocationComponent,
+    AddLocationComponent
   ],
   imports: [
     BrowserModule,
