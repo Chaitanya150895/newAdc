427d081e724899695e46a4d5fea8eca2f8a1b548
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-08-21 15:32:47 +0530

    location table added

--- src/app/add-location/new-location/new-location.component.ts
+++ src/app/add-location/new-location/new-location.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { FormArray } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Accept': 'application/json'
+  })
+};
 
 @Component({
   selector: '[app-new-location]',
@@ -7,9 +17,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewLocationComponent implements OnInit {
 
-  constructor() { }
+  regions = [];
+  location_types = [];
+
+  locationForm = new FormGroup({
+    name: new FormControl(''),
+    address: new FormControl(''),
+    city: new FormControl(''),
+    state: new FormControl(''),
+    zipcode: new FormControl(''),
+    location_number: new FormControl(''),
+    region_id: new FormControl(''),
+    location_type_id: new FormControl('')
+  });
+
+  constructor(private http: HttpClient) {
+    this.http.get("http://localhost/logistic_v1/api/regions.json").subscribe(data => {
+      console.log(data);
+      this.regions = data['data'];
+    });
+
+    this.http.get("http://localhost/logistic_v1/api/location_types.json").subscribe(data => {
+      console.log(data);
+      this.location_types = data['data'];
+    });
+  }
 
   ngOnInit() {
   }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.locationForm.value);
+
+    this.http.post("http://localhost/logistic_v1/api/locations.json", this.locationForm.value, httpOptions)
+    .pipe(
+    ).subscribe(data => {
+      console.log(data);
+    });
+  }
+
 
 }
