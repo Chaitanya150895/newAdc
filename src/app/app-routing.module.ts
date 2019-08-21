718fa295e89195c0f25109b1e124a32561abb328
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-08-21 16:04:14 +0530

    cangeds

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddLocationComponent } from './add-location/add-location.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AddLocationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewLocationComponent } from './add-location/new-location/new-location.component';
+import { AddLocationComponent } from './add-location/add-location.component';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
+import { SidebarComponent } from './main-sidebar/sidebar/sidebar.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
+import { ControlSidebarComponent } from './control-sidebar/control-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewLocationComponent,
+    AddLocationComponent,
+    MainHeaderComponent,
+    MainSidebarComponent,
+    SidebarComponent,
+    MainFooterComponent,
+    ControlSidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
