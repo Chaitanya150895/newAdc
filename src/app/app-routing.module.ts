c63692c45fadede2a78ccebe784b77be3d160e3c
Vanita Bhoyar <vanita.bhoyar282@gmail.com>
2019-08-23 03:09:37 -0700

    Added User Component And Validation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { AddUserComponent } from './add-user/add-user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:DashboardComponent},
+  { path: 'userlist', component: UserListComponent },
+  { path: 'adduser', component: AddUserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
+import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+import { HttpClientModule }    from '@angular/common/http';
+import { AddUserComponent } from './add-user/add-user.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    MainHeaderComponent,
+    MainSidebarComponent,
+    ContentWrapperComponent,
+    MainFooterComponent,
+    UserListComponent,
+    AddUserComponent,
+    DashboardComponent
+  ],  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,    
+    FormsModule,
+    ReactiveFormsModule, 
+    RouterModule.forRoot([
+      { path: '', component: DashboardComponent },
+      { path: 'userlist', component: UserListComponent },
+      { path: 'adduser', component: AddUserComponent }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
