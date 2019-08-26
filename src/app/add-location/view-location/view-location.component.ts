bcf7b7e49ed7ac12bbed06b4bbfa58397398dd06
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-08-26 17:37:53 +0530

    loader added to the view-location

--- src/app/add-location/view-location/view-location.component.ts
+++ src/app/add-location/view-location/view-location.component.ts
@@ -11,22 +11,25 @@ import { ActivatedRoute } from '@angular/router';
 export class ViewLocationComponent implements OnInit {
   location;
   locationForm: any;
+  loading = false;
 
   constructor(private http: HttpClient, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-
+    this.loading = true;
     this.route.paramMap.subscribe(params => {
       let id = params.get('locationId');
 
       this.http.get("http://localhost/logistic_v1/api/locations/" + id + ".json").subscribe(data => {
-
+        this.loading = false;
         console.log(`${id}`);
 
         this.location = data['data'];
-      
-console.log(this.location);
+
+        
+
+        console.log(this.location);
         // this.locationForm.setValue(this.location);
 
       });
