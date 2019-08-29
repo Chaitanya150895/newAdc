93b4bd4916539e4235784a44a0970ab9b1e9faed
Ashish Kumar Patel <ashish.onmobile@gmail.com>
2019-08-29 15:32:08 +0530

    created dynamic menu

--- src/app/main-sidebar/sidebar/sidebar.component.ts
+++ src/app/main-sidebar/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: '[app-sidebar]',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  menus = [];
+  constructor(private http:HttpClient) { 
+
+    this.http.get("http://localhost/logistic_v1/api/menus.json").subscribe(data => {
+
+    console.log(data['data']);
+    this.menus = data['data'];
+
+    })
+  }
 
   ngOnInit() {
   }
