941795d56f55aec6208b8a916833fcbc43d7729b
Ashish Kumar Patel <ashish.onmobile@gmail.com>
2019-09-07 13:13:57 +0530

    added login component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LocationListComponent } from './location/location-list/location-list.component';
-import { NewLocationComponent } from './location/new-location/new-location.component';
-import { EditLocationComponent } from './location/location-list/edit-location/edit-location.component';
-import { ViewLocationComponent } from './location/location-list/view-location/view-location.component';
+import { LocationListComponent } from './locations/location-list/location-list.component';
+import { NewLocationComponent } from './locations/new-location/new-location.component';
+import { EditLocationComponent } from './locations/location-list/edit-location/edit-location.component';
+import { ViewLocationComponent } from './locations/location-list/view-location/view-location.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserListComponent } from './user/user-list/user-list.component';
-import { AddUserComponent } from './user/add-user/add-user.component';
-import { EditUserComponent } from './user/edit-user/edit-user.component';
-import { ViewUserComponent } from './user/view-user/view-user.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { AddUserComponent } from './users/add-user/add-user.component';
+import { EditUserComponent } from './users/edit-user/edit-user.component';
+import { ViewUserComponent } from './users/view-user/view-user.component';
 import { InventoryListComponent } from './inventory/inventory-list/inventory-list.component';
+import { LoginComponent } from './users/login/login.component';
 
 const routes: Routes = [
 
   // { path: "", component: DashboardComponent },
 
   { path: '', component: DashboardComponent },
+  { path: 'users/login', component: LoginComponent },
   { path: 'users', component: UserListComponent },
   { path: 'users/add', component: AddUserComponent },
   { path: 'users/edit/:userId', component: EditUserComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NewLocationComponent } from './location/new-location/new-location.component';
+import { NewLocationComponent } from './locations/new-location/new-location.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
 import { MainFooterComponent } from './main-footer/main-footer.component';
-import { LocationListComponent } from './location/location-list/location-list.component';
+import { LocationListComponent } from './locations/location-list/location-list.component';
 import { RouterModule } from '@angular/router';
-import { EditLocationComponent } from './location/location-list/edit-location/edit-location.component';
-import { ViewLocationComponent } from './location/location-list/view-location/view-location.component';
+import { EditLocationComponent } from './locations/location-list/edit-location/edit-location.component';
+import { ViewLocationComponent } from './locations/location-list/view-location/view-location.component';
 import { PipeTransformPipe } from './pipe-transform.pipe';
 import { ReplaceUnderscorePipe } from './replace-underscore.pipe';
 import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
-import { LocationComponent } from './location/location.component';
-import { UserListComponent } from './user/user-list/user-list.component';
-import { AddUserComponent } from './user/add-user/add-user.component';
-import { EditUserComponent } from './user/edit-user/edit-user.component';
-import { ViewUserComponent } from './user/view-user/view-user.component';
+import { LocationComponent } from './locations/location.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { AddUserComponent } from './users/add-user/add-user.component';
+import { EditUserComponent } from './users/edit-user/edit-user.component';
+import { ViewUserComponent } from './users/view-user/view-user.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { InventoryListComponent } from './inventory/inventory-list/inventory-list.component';
-import { SchedulesComponent } from './location/new-location/schedules/schedules.component';
+import { SchedulesComponent } from './locations/new-location/schedules/schedules.component';
+import { LoginComponent } from './users/login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -46,6 +47,7 @@ import { SchedulesComponent } from './location/new-location/schedules/schedules.
     ReplaceUnderscorePipe,
     InventoryListComponent,
     SchedulesComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
