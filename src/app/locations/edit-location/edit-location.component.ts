7a8c395723c7a5d8f99ee453df423581f916f8d9
Ashish Kumar Patel <ashish.onmobile@gmail.com>
2019-09-14 23:45:24 +0530

    updated code for add and edit.

--- src/app/locations/edit-location/edit-location.component.ts
+++ src/app/locations/edit-location/edit-location.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/http.service';
-import { FormBuilder, FormArray, FormGroup } from '@angular/forms';
+import { FormBuilder, FormArray, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -10,8 +10,8 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EditLocationComponent implements OnInit {
 
-  REGION_INDEX = 5;
-  LOCATION_TYPE_INDEX = 6;
+  REGION_INDEX = 6;
+  LOCATION_TYPE_INDEX = 7;
   regions=[];
 
   defaultSchdeuls = [
@@ -25,25 +25,25 @@ export class EditLocationComponent implements OnInit {
   ]
 
 formData = [
- // {for : "id",control:"input" , type:"text",label:"ID",placeholder:"Enter Name", id:"id",control_name:"id"},
   {for : "name",control:"input" , type:"text",label:"Name",placeholder:"Enter Name", id:"name",control_name:"name"},
   {for : "address", control:"input" ,type:"text",label:"Address",placeholder:"Enter Address",id:"address",control_name:"address"},
   {for : "city", control:"input" ,type:"text",label:"City",placeholder:"Enter City",id:"city",control_name:"city"},
   {for : "state", control:"input" ,type:"text",label:"State",placeholder:"Enter State",id:"state",control_name:"state"},
   {for : "location_number", control:"input" ,type:"text",label:"Location Number",placeholder:"Enter Location Number",id:"location_number",control_name:"location_number"},
+  {for : "trailer_bays", control:"input" ,type:"number",label:"Trailer Bays",placeholder:"Enter Trailer Bays",id:"trailer_bays",control_name:"trailer_bays"},
   {for : "region", control:"select" ,type:null,label:"Region",placeholder:"Select Region",id:"region_id",control_name:"region_id",array:null},
   {for : "location_type", control:"select" ,type:null,label:"Location Type",placeholder:"Select Location Type",id:"location_type_id",control_name:"location_type_id",array:null}
 ]
 
-  
 
   customForm = this.fb.group({
     id:[''],
-    name: [''],
-    address: [''],
+    name: ['',[Validators.required, Validators.minLength(4)]],
+    address: ['',[Validators.required, Validators.minLength(4)]],
     city: [''],
     state: [''],
     location_number:[''],
+    trailer_bays:[''],
     region_id:[''],
     location_type_id:[''],
     schedules:this.fb.array([
