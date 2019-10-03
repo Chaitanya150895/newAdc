f91f4a5fb0e43449b7a42cf81cacd89865572ea3
pallavi-bande <bandepallavib@gmail.com>
2019-10-03 15:49:40 +0530

    sections seperated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,69 +1,92 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LocationListComponent } from './locations/location-list/location-list.component';
-import { NewLocationComponent } from './locations/new-location/new-location.component';
-import { EditLocationComponent } from './locations/edit-location/edit-location.component';
-import { ViewLocationComponent } from './locations/location-list/view-location/view-location.component';
+
+//AdminLte
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+//Users
 import { UserListComponent } from './users/user-list/user-list.component';
 import { AddUserComponent } from './users/add-user/add-user.component';
 import { EditUserComponent } from './users/edit-user/edit-user.component';
 import { ViewUserComponent } from './users/view-user/view-user.component';
-import { InventoryListComponent } from './inventory/inventory-list/inventory-list.component';
-import { LoginComponent } from './users/login/login.component';
+import { ForgotPasswordComponent } from './users/forgot-password/forgot-password.component';
 import { LoginPageComponent } from './users/login-page/login-page.component';
 
-import { NeedAuthGuard } from './auth.guard';
-import { AddLocationComponent } from './locations/add-location/add-location.component';
-import { TrailerListComponent } from './trailers/trailer-list/trailer-list.component';
-import { AddTrailerComponent } from './trailers/add-trailer/add-trailer.component';
-
+//Inventories
+import { InventoryListComponent } from './inventory/inventory-list/inventory-list.component';
 import { AddInventoryComponent } from './inventory/add-inventory/add-inventory.component';
 import { EditInventoryComponent } from './inventory/edit-inventory/edit-inventory.component';
 import { ViewInventoryComponent } from './inventory/view-inventory/view-inventory.component';
 
+
+//Locations
+import { AddLocationComponent } from './locations/add-location/add-location.component';
+import { LocationListComponent } from './locations/location-list/location-list.component';
+import { EditLocationComponent } from './locations/edit-location/edit-location.component';
+import { ViewLocationComponent } from './locations/location-list/view-location/view-location.component';
+
+//orders
 import { AddOrderComponent } from './orders/add-order/add-order.component';
 import { ViewOrderComponent } from './orders/order-list/view-order/view-order.component';
 import { EditOrderComponent } from './orders/edit-order/edit-order.component';
 import { OrderListComponent } from './orders/order-list/order-list.component';
+
+
+//products
 import { AddProductComponent } from './products/add-product/add-product.component';
 import { EditProductComponent } from './products/edit-product/edit-product.component';
 import { ViewProductComponent } from './products/product-list/view-product/view-product.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
+
+//stores
 import { StoreComponent } from './store/store.component';
-import { ForgotPasswordComponent } from './users/forgot-password/forgot-password.component';
+
+//trailers
 import { TrailersComponent } from './trailers/trailers.component';
+
+//Pipes
+import { NeedAuthGuard } from './auth.guard';
 
 const routes: Routes = [
 
   // { path: "", component: DashboardComponent },
 { path:'',component:DashboardComponent,
   children: [
-
-
-
+    //trailers
     { path: 'trailers', component: TrailersComponent , canActivate:[NeedAuthGuard] },
+
+    //orders
     { path:'orders/add', component: AddOrderComponent,canActivate:[NeedAuthGuard]},
     { path: 'orders/view/:orderId', component: ViewOrderComponent , canActivate:[NeedAuthGuard] },
     { path: 'orders/edit/:orderId', component: EditOrderComponent , canActivate:[NeedAuthGuard] },
     { path:'orders', component: OrderListComponent,canActivate:[NeedAuthGuard]},
+
+    //products
     { path:'products/add', component: AddProductComponent,canActivate:[NeedAuthGuard]},
     { path: 'products/edit/:productId', component: EditProductComponent , canActivate:[NeedAuthGuard] },
     {path:'products/view/:productId', component:ViewProductComponent,canActivate:[NeedAuthGuard] },
     { path:'products', component: ProductListComponent,canActivate:[NeedAuthGuard]},
+
+    //users
     { path: 'users', component: UserListComponent , canActivate:[NeedAuthGuard] },
     { path: 'users/add', component: AddUserComponent , canActivate:[NeedAuthGuard] },
     { path: 'users/edit/:userId', component: EditUserComponent , canActivate:[NeedAuthGuard] },
     { path: 'users/view/:userId', component: ViewUserComponent , canActivate:[NeedAuthGuard] },
+
+    //locations
     { path: 'locations/add', component: AddLocationComponent , canActivate:[NeedAuthGuard] },
     { path: 'locations', component: LocationListComponent, canActivate:[NeedAuthGuard] },
     { path: 'locations/edit/:locationId', component: EditLocationComponent , canActivate:[NeedAuthGuard] },
     { path: 'locations', component: LocationListComponent, canActivate:[NeedAuthGuard] },
     { path: 'locations/view/:locationId', component: ViewLocationComponent , canActivate:[NeedAuthGuard] },
+
+    //Inventories
     { path: 'inventories', component: InventoryListComponent , canActivate:[NeedAuthGuard] },
     { path: 'inventories/add', component: AddInventoryComponent , canActivate:[NeedAuthGuard] },
     { path: 'inventories/edit/:inventoryId', component: EditInventoryComponent , canActivate:[NeedAuthGuard] },
     { path: 'inventories/view/:inventoryId', component: ViewInventoryComponent , canActivate:[NeedAuthGuard] },
+
+    //stores
      { path: 'store', component: StoreComponent, canActivate: [NeedAuthGuard] },
   ],canActivate:[NeedAuthGuard]
 },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,115 +1,157 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NewLocationComponent } from './locations/new-location/new-location.component';
+//@angular
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
+import { DateTimePickerModule } from '@syncfusion/ej2-angular-calendars';
+
+
+//AdminLte
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
 import { MainFooterComponent } from './main-footer/main-footer.component';
+import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MainComponent } from './home/main/main.component';
+
+
+//locations
+import { NewLocationComponent } from './locations/new-location/new-location.component';
 import { LocationListComponent } from './locations/location-list/location-list.component';
 import { EditLocationComponent } from './locations/edit-location/edit-location.component';
 import { ViewLocationComponent } from './locations/location-list/view-location/view-location.component';
-import { PipeTransformPipe } from './pipe-transform.pipe';
-import { ReplaceUnderscorePipe } from './replace-underscore.pipe';
-import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
 import { LocationComponent } from './locations/location.component';
+import { SchedulesComponent } from './locations/new-location/schedules/schedules.component';
+import { AddLocationComponent } from './locations/add-location/add-location.component';
+
+//users
 import { UserListComponent } from './users/user-list/user-list.component';
 import { AddUserComponent } from './users/add-user/add-user.component';
 import { EditUserComponent } from './users/edit-user/edit-user.component';
 import { ViewUserComponent } from './users/view-user/view-user.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { InventoryListComponent } from './inventory/inventory-list/inventory-list.component';
-import { SchedulesComponent } from './locations/new-location/schedules/schedules.component';
-import { LoginComponent } from './users/login/login.component';
-import { MainComponent } from './home/main/main.component';
 import { LoginPageComponent } from './users/login-page/login-page.component';
-import { NeedAuthGuard } from './auth.guard';
-import { AddLocationComponent } from './locations/add-location/add-location.component';
-import { AddTrailerComponent } from './trailers/add-trailer/add-trailer.component';
-import { EditTrailerComponent } from './trailers/edit-trailer/edit-trailer.component';
-import { TrailerListComponent } from './trailers/trailer-list/trailer-list.component';
+import { ForgotPasswordComponent } from './users/forgot-password/forgot-password.component';
+
+
+//inventory
+import { InventoryListComponent } from './inventory/inventory-list/inventory-list.component';
 import { AddInventoryComponent } from './inventory/add-inventory/add-inventory.component';
 import { EditInventoryComponent } from './inventory/edit-inventory/edit-inventory.component';
 import { ViewInventoryComponent } from './inventory/view-inventory/view-inventory.component';
 
-import { CommonModule } from '@angular/common';
-import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
 
-import { DateTimePickerModule } from '@syncfusion/ej2-angular-calendars';
-import { ForgotPasswordComponent } from './users/forgot-password/forgot-password.component';
+//products
+import { AddProductComponent } from './products/add-product/add-product.component';
+import { EditProductComponent } from './products/edit-product/edit-product.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ViewProductComponent } from './products/product-list/view-product/view-product.component';
+
+
+//orders
+import { AddOrderComponent } from './orders/add-order/add-order.component';
+import { EditOrderComponent } from './orders/edit-order/edit-order.component';
+import { OrderListComponent } from './orders/order-list/order-list.component';
+import { ViewOrderComponent } from './orders/order-list/view-order/view-order.component';
+
+//trailers
+import { AddTrailerComponent } from './trailers/add-trailer/add-trailer.component';
+import { TrailerListComponent } from './trailers/trailer-list/trailer-list.component';
+import { TrailersComponent } from './trailers/trailers.component';
+
+//status
 import { StatusComponent } from './status/status/status.component';
 import { DocksComponent } from './status/docks/docks.component';
 import { YardsComponent } from './status/yards/yards.component';
+
+
+//stores
 import { StoreComponent } from './store/store.component';
 import { ProductInventoryComponent } from './store/product-inventory/product-inventory.component';
 import { OutboundInventoryComponent } from './store/outbound-inventory/outbound-inventory.component';
 import { BackroomInventoryComponent } from './store/backroom-inventory/backroom-inventory.component';
 import { DeliveryScheduleComponent } from './store/delivery-schedule/delivery-schedule.component';
-import { AddOrderComponent } from './orders/add-order/add-order.component';
-import { EditOrderComponent } from './orders/edit-order/edit-order.component';
-import { OrderListComponent } from './orders/order-list/order-list.component';
-import { ViewOrderComponent } from './orders/order-list/view-order/view-order.component';
-import { AddProductComponent } from './products/add-product/add-product.component';
-import { EditProductComponent } from './products/edit-product/edit-product.component';
-import { ProductListComponent } from './products/product-list/product-list.component';
-import { ViewProductComponent } from './products/product-list/view-product/view-product.component';
-import { ViewTrailerComponent } from './trailers/trailer-list/view-trailer/view-trailer.component';
-import { TrailersComponent } from './trailers/trailers/trailers.component';
+
+
+//pipes
+import { PipeTransformPipe } from './pipe-transform.pipe';
+import { ReplaceUnderscorePipe } from './replace-underscore.pipe';
+import { NeedAuthGuard } from './auth.guard';
+
+
 @NgModule({
   declarations: [
+    //App
     AppComponent,
-    NewLocationComponent,
+
+    //AdminLte
     MainHeaderComponent,
     MainSidebarComponent,
     MainFooterComponent,
+    DashboardComponent,
+    MainComponent,
+    ContentWrapperComponent,
+
+    //Locations
+    LocationComponent,
+    NewLocationComponent,
     LocationListComponent,
     EditLocationComponent,
     ViewLocationComponent,
-    PipeTransformPipe,
-    ReplaceUnderscorePipe,
-    ContentWrapperComponent,
-    LocationComponent,
+    AddLocationComponent,
+    SchedulesComponent,
+    
+    //Users
     UserListComponent,
     AddUserComponent,
-    DashboardComponent,
     EditUserComponent,
     ViewUserComponent,
-    ReplaceUnderscorePipe,
-    InventoryListComponent,
-    SchedulesComponent,
-    LoginComponent,
-    MainComponent,
     LoginPageComponent,
-    AddLocationComponent,
-    AddTrailerComponent,
-    EditTrailerComponent,
-    TrailerListComponent,
+    ForgotPasswordComponent,
+
+    //Inventories
+    InventoryListComponent,
     AddInventoryComponent,
     EditInventoryComponent,
     ViewInventoryComponent,
-    ForgotPasswordComponent,
+
+    //Trailers
+    AddTrailerComponent,
+    TrailerListComponent,
+    TrailersComponent,
+
+
+    //Status
     StatusComponent,
     DocksComponent,
     YardsComponent,
+
+    //Orders
     EditOrderComponent,
     OrderListComponent,
     ViewOrderComponent,
+    AddOrderComponent,
+
+    //Products
     AddProductComponent,
     EditProductComponent,
     ProductListComponent,
     ViewProductComponent,
-    TrailerListComponent,
-    ViewTrailerComponent,
+    
+    //Stores
     StoreComponent,
     ProductInventoryComponent,
     OutboundInventoryComponent,
     BackroomInventoryComponent,
     DeliveryScheduleComponent,
-    AddOrderComponent,
-    TrailersComponent
+
+    //pipes
+    PipeTransformPipe,
+    ReplaceUnderscorePipe,
+    ReplaceUnderscorePipe
   ],
   imports: [
     BrowserModule,
