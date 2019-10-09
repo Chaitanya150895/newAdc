3250af120b9a09ca137c062bc6f13e01dd73196b
pallavi-bande <bandepallavib@gmail.com>
2019-10-09 15:32:43 +0530

    dropdownlist for edit tariler status

--- src/app/trailers/trailer-list/trailer-list.component.ts
+++ src/app/trailers/trailer-list/trailer-list.component.ts
@@ -16,6 +16,7 @@ export class TrailerListComponent implements OnInit {
     "Trailer Statuses",
     "Action"
   ]
+  trailer_statuses=[];
 
 
   constructor( private httpService: HttpService) {
@@ -23,12 +24,16 @@ export class TrailerListComponent implements OnInit {
   }
   ngOnInit() {
     this.httpService.getHttp("trailers.json").subscribe( data => {
-
       console.log(data);
-
       this.trailers = data['data'];
-
     });
+
+    this.httpService.getHttp("trailer_statuses.json").subscribe(data => {
+      console.log(data);
+    // this.TRAILER_STATUSES_INDEX = ( data['data']);
+    this.trailer_statuses = data['data'];
+    });
+
   }
   reloadData(){
     this.httpService.getHttp("trailers.json").subscribe( data => {
@@ -37,6 +42,22 @@ export class TrailerListComponent implements OnInit {
 
       this.trailers = data['data'];
     });
+  }
+  setCodeEdit(index){
+    this.trailers.forEach(p => p.canEditCode = false)
+    this.trailers[index].canEditCode=true 
+ }
+ save(index){
+  let trailer = this.trailers[index];
+ 
+   this.httpService.putHttp("trailers/"+trailer.id+".json",JSON.stringify(trailer)).subscribe(data => {
+     //this.loading = false;
+     console.log(data);
+     //this.products = data['data'];
+     trailer.canEditCode = false;
+   });
+   console.log(this.trailers[index]);
+  
   }
   deleteTrailer(id, trailerId) {
       this.httpService.deleteHttp("trailers/"+id+".json").subscribe(data => {
